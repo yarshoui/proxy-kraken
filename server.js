fc3aebd2bf789b6fb7436dd5be46c0f1f47f3130
Ilya <ilia.yershov@gmail.com>
2020-01-13 00:06:39 +0100

    Update to dedicated endpoint

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var config = {
 
 app.use(requestTime);
 
-app.get('/', function (req, res) {
+app.get('/api', function (req, res) {
     const pair = req.query.pair || 'xbtusd';
   axios.get(`https://api.kraken.com/0/public/Depth?pair=${pair}&count=4`, config).then((response) => {
     const pair = Object.keys(response.data.result)[0];
