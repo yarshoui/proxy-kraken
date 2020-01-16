27890a226d6d5458bdab88396dcd4cd1c2192d0e
Ilya <ilia.yershov@gmail.com>
2020-01-17 00:02:17 +0100

    Implementin console log of the table

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get('/*', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 })
 
-app.get('/index', (req, res) => {
+app.get('/js', (req, res) => {
   res.sendFile(__dirname + '/index.js');
 })
 
