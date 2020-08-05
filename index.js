0766e3cfbe27c09778e3f31df0ab7527f9879681
yarshoui <ilia.yershov@gmail.com>
2020-08-05 18:05:07 +0200

    Tables update change

--- index.js
+++ index.js
@@ -1,25 +1,128 @@
-window.onload = () => {
-    const data = {
-      as: [],
-      bs: [],
+const data = {
+  as: [],
+  bs: [],
+};
+let ws;
+let timeOfClose = 0;
+
+let qtyFilterInput;
+let pairFilterInput;
+let tableAskBody;
+let tableBidBody;
+
+const subscribeToCurrencyPair = () => {
+        // console.log(pairFilterInput.value);
+
+        const payload = {
+            event: 'subscribe',
+            pair: [
+                pairFilterInput.value,
+            ],
+            subscription: {
+                // interval: 100,
+                depth: 1000,
+                name: 'book',
+            }
+        };
+
+        // console.log(payload);
+        return JSON.stringify(payload);
     };
+
+function resetData(data) {
+    data.as = [];
+    data.bs = [];
+}
+
+function sendSubscription() {
+    ws.send(subscribeToCurrencyPair())
+}
+
+function prepareSocket() {
+    ws = new WebSocket('wss://ws.kraken.com');
+
+    ws.onclose = (data) => {
+        // todo reset if failed 5 times in 30-60second
+        // timeOfClose = timeOfClose === 0 ? Date.now() timeOfClose
+        // if timeOfClose - Date.now() > 60000 alert()
+
+        prepareSocket();
+        console.log('WebSocket is closed now.', data);
+    };
+
+    ws.onopen = () => {
+        console.log('[open] Connection established 1');
+        setInterval(sendSubscription, 3000);  
+        ws.send(subscribeToCurrencyPair());
+    };
+
+    ws.onerror = (error) => {
+        console.log(`[error] ${error.message}`);
+    };
+
+    ws.onmessage = function(msg) {
+      const newData = JSON.parse(msg.data);
+
+      if (newData.channelID || newData.connectionID || !Array.isArray(newData)
+        || newData.length < 2 || !newData[1]
+      ) {
+          return;
+      }
+
+// bid update
+      if (newData[1].b) {
+          // if bid is 0 - find and remove from initial data
+          // data.bs = [ ...data.bs, ...newData[1].b ];
+          return;
+      }
+
+      // ask update
+      if (newData[1].a) {
+          // if ask is 0 - find and remove from initial data
+          // data.as = [ ...data.as, ...newData[1].a ];
+          return;
+      }
+
+      // something which is not valid for us
+      if (!newData[1].as || !newData[1].bs) {
+          return;
+      }
+
+      resetData(data);
+
+      console.count('onmessage');
+      console.log(newData[1]);
+      // update initial ask/bid array(1000 elements)
+      data.as = newData[1].as;
+      data.bs = newData[1].bs;
+    };
+}
+
+window.onload = () => {
     // const apiUrl = 'https://proxy-kraken.herokuapp.com/api';
-    const ws = new WebSocket('wss://ws.kraken.com');
+
     /* const timeUpdateInput = document.getElementById('timeupdate');
     const timeUpdate = (timeUpdateInput.value)*1000;*/
-    const qtyFilterInput = document.getElementById('qtyfilter');
-    const pairFilterInput = document.getElementById('pairfilter');
-    const tableAskBody = document.querySelector('.ask tbody');
-    const tableBidBody = document.querySelector('.bid tbody');
+    qtyFilterInput = document.getElementById('qtyfilter');
+    pairFilterInput = document.getElementById('pairfilter');
+    tableAskBody = document.querySelector('.ask tbody');
+    tableBidBody = document.querySelector('.bid tbody');
 
     // send signal to socket to change pair
     pairFilterInput.addEventListener('change', (evt) => {
         console.log(evt.srcElement.value);
-        ws.send(subscribeToCurrencyPair());
+        sendSubscription();
         // clear cached data
-        data.as = [];
-        data.bs = [];
+//         resetData(data);
     });
+
+// if we want to update data after qty change
+//     qtyFilterInput.addEventListener('change', (evt) => {
+//         console.log(evt.srcElement.value);
+//         sendSubscription();
+//         // clear cached data
+//         //  resetData(data);
+//     });
 
     /**
      * @description replace table data in ask and bid tables
@@ -75,73 +178,6 @@ window.onload = () => {
      * @description data for socket, to subscribe to some pair
      * @axample pair: XBT/USD
      */
-    const subscribeToCurrencyPair = () => {
-        // console.log(pairFilterInput.value);
-
-        const payload = {
-            event: 'subscribe',
-            pair: [
-                pairFilterInput.value,
-            ],
-            subscription: {
-                // interval: 100,
-                depth: 1000,
-                name: 'book',
-            }
-        };
-
-        // console.log(payload);
-
-        return JSON.stringify(payload);
-    };
-
-    // const interval = setInterval(() => {
-    //    getData();
-    // }, 3000);
-    ws.onclose = (data) => {
-        console.log('WebSocket is closed now.', data);
-    };
-
-    ws.onopen = () => {
-        console.log('[open] Connection established 1');
-        ws.send(subscribeToCurrencyPair());
-    };
-
-    ws.onerror = (error) => {
-        console.log(`[error] ${error.message}`);
-    };
-
-    ws.onmessage = function(msg) {
-      const newData = JSON.parse(msg.data);
-
-      if (newData.channelID || newData.connectionID || !Array.isArray(newData)
-        || newData.length < 2 || !newData[1]
-      ) {
-          return;
-      }
-
-      // bid update
-      if (newData[1].b) {
-          // if bid is 0 - find and remove from initial data
-          // data.bs = [ ...data.bs, ...newData[1].b ];
-          return;
-      }
-
-      // ask update
-      if (newData[1].a) {
-          // if ask is 0 - find and remove from initial data
-          // data.as = [ ...data.as, ...newData[1].a ];
-          return;
-      }
-
-      // something which is not valid for us
-      if (!newData[1].as || !newData[1].bs) {
-          return;
-      }
-
-      console.log(newData[1]);
-      // update initial ask/bid array(1000 elements)
-      data.as = [ ...data.as, ...newData[1].as ];
-      data.bs = [ ...data.bs, ...newData[1].bs ];
-    };
+    prepareSocket();
+/// todo: prepare socket
 };
